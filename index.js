b2d53fba3d2e743a7a8df9a029f7cc399fb80449
Claire ANOT DELCOURT <claire.anotd@gmail.com>
2024-09-20 10:48:17 +0200

    new object

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Afficher qqch en console
 // console.log("Hello world !")
 
+/*
+// Premiers tests
 let name = "Bob"
 const age = 33
 
@@ -9,3 +11,20 @@ name = "Michel"
 
 console.log(name, age, typeof name, typeof age)
 console.log(name.toUpperCase(), name.length)
+*/
+
+/*
+// array
+let users = ["Michel", "Bob", "Julie"]
+users.push("TestPush")
+
+console.log(users, users.length, users[1])
+*/
+
+// object
+let user = {
+    name: "Bob",
+    age: 23,
+}
+user.age += 1
+console.log(user, user.name, user['age'])
