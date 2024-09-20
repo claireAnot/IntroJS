29c2a2ef13e524dd5940f44caef2a63b5300054b
Claire ANOT DELCOURT <claire.anotd@gmail.com>
2024-09-20 09:42:29 +0200

    test

---
+++ index.js
@@ -0,0 +1,11 @@
+// Afficher qqch en console
+// console.log("Hello world !")
+
+let name = "Bob"
+const age = 33
+
+name = "Michel"
+// age = 10 -> impossible de modifier une 'const'
+
+console.log(name, age, typeof name, typeof age)
+console.log(name.toUpperCase(), name.length)
